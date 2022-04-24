2378a14a9721cd5ce1033772bce3dfc319ddb065
Myrom <webmyrom@gmail.com>
2022-04-24 01:04:08 -0300

    repos

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { useState } from 'react';
+import RepositorioItem from './RepositorioItem';
+import api from '../services/api';
+
+function App() {
+  const [pesquisar, setPesquisar] = useState('');
+  const [repoData, setrepoData] = useState(null);
+const numero = 0;
+  async function buscaRepositorio() {
+
+    const response = await api.get(`repos/${pesquisar}`)
+      .catch(()=>{ 
+        alert("Dados Não encontrados!!") 
+      });
+        setrepoData(response.data);
+  }
+
+  return (
+    <div className="App">
+      <div className="Pesquisar">
+        <input
+          value={pesquisar}
+          onChange={(e)=>{ setPesquisar(e.target.value)}}
+          placeholder="Escreva aqui"></input>
+        
+        <button onClick={buscaRepositorio}>Pesquisar</button>
+      </div>
+      {repoData &&
+        <RepositorioItem data={repoData}/>
+      }
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/RepositorioItem.js
@@ -0,0 +1,15 @@
+
+import './RepositorioItem.css';
+
+function RepositorioItem(props) {
+  return (
+    <div className="RepositorioItem">
+      <img src={props.data.owner.avatar_url} alt='avatar'></img>
+      <h1>{props.data.name}</h1>
+      <h1>{props.data.description}</h1>
+      
+    </div>
+  );
+}
+
+export default RepositorioItem;
